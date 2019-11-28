336c28505a60b4f02526f02de345f48fa7df88ea
Mai Mahmoud <maikenawy@hotmail.com>
2019-11-28 18:50:12 +0200

    [Preprocessing] Add another method to correct Skew

--- Lines.py
+++ Lines.py
@@ -2,6 +2,13 @@
 #https://stackoverflow.com/questions/34981144/split-text-lines-in-scanned-document
 import cv2
 import numpy as np
+from scipy.ndimage import interpolation as inter
+
+def find_score(arr, angle):
+    data = inter.rotate(arr, angle, reshape=False, order=0)
+    hist = np.sum(data, axis=1)
+    score = np.sum((hist[1:] - hist[:-1]) ** 2)
+    return hist, score
 
 def LineSegmentation(img, saveResults=True):
 
@@ -31,16 +38,38 @@ def LineSegmentation(img, saveResults=True):
 
 
     ## (4) Find rotated matrix, do rotation
-    M = cv2.getRotationMatrix2D((cx,cy), ang, 1.0)
-    rotated = cv2.warpAffine(threshed, M, (img.shape[1], img.shape[0]))
-    pts = cv2.findNonZero(rotated)
-    ret = cv2.minAreaRect(pts)
-    # print(ret)
+    # ################## Method 1 ##################
+    # M = cv2.getRotationMatrix2D((cx,cy), ang, 1.0)
+    # rotated = cv2.warpAffine(threshed, M, (img.shape[1], img.shape[0]))
+    # pts = cv2.findNonZero(rotated)
+    # ret = cv2.minAreaRect(pts)
+    
+    # ################## Method 2 ##################
+    skewImage = threshed
+    ht, wd = skewImage.shape
+    bin_img = 1 - (threshed.reshape((ht, wd)) / 255.0)
+
+    delta = 1
+    limit = 5
+    angles = np.arange(-limit, limit+delta, delta)
+    scores = []
+    for angle in angles:
+        hist, score = find_score(bin_img, angle)
+        scores.append(score)
+
+    best_score = max(scores)
+    best_angle = angles[scores.index(best_score)]
+    
+    # correct skew
+    data = inter.rotate(bin_img, best_angle, reshape=False, order=0, cval = 1)
+    rotatedImage = 255*data
+    rotated = 255 - rotatedImage
+    
     # cv2.imshow('original',threshed)
     # cv2.imshow('rotated',rotated)
     # cv2.waitKey(0)    
+    ###################################    
     
-    # rotated=255-rotated
     ## (5) find and draw the upper and lower boundary of each lines
     hist = cv2.reduce(rotated,1, cv2.REDUCE_AVG).reshape(-1)
     # print(hist)
@@ -71,4 +100,4 @@ def LineSegmentation(img, saveResults=True):
 
     return lines
 
-# LineSegmentation(cv2.imread("Dataset/scanned/capr377.png"),True)
+# LineSegmentation(cv2.imread("Dataset/scanned/capr3.png"),True)
